2234772464c04a43a57c38974e895966049f62c6
waxie <97557771+waxiechc@users.noreply.github.com>
2024-10-16 11:59:59 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class TRTriangle
+{
+    private double sideLength;
+
+    public TRTriangle()
+    {
+        sideLength = 1.0;
+    }
+
+    public TRTriangle(double sideLength)
+    {
+        this.sideLength = sideLength;
+    }
+
+    public TRTriangle(TRTriangle triangle)
+    {
+        this.sideLength = triangle.sideLength;
+    }
+
+    public void InputData()
+    {
+        Console.Write("Введiть довжину сторони: ");
+        sideLength = Convert.ToDouble(Console.ReadLine());
+    }
+
+    public void OutputData()
+    {
+        Console.WriteLine($"Довжина сторони трикутника: {sideLength}");
+    }
+
+    public double GetArea()
+    {
+        return (Math.Sqrt(3) / 4) * Math.Pow(sideLength, 2);
+    }
+
+    public double GetPerimeter()
+    {
+        return 3 * sideLength;
+    }
+
+    public bool IsEqual(TRTriangle otherTriangle)
+    {
+        return this.sideLength == otherTriangle.sideLength;
+    }
+
+    public static TRTriangle operator +(TRTriangle a, TRTriangle b)
+    {
+        return new TRTriangle(a.sideLength + b.sideLength);
+    }
+
+    public static TRTriangle operator -(TRTriangle a, TRTriangle b)
+    {
+        return new TRTriangle(a.sideLength - b.sideLength);
+    }
+
+    public static TRTriangle operator *(TRTriangle a, double scalar)
+    {
+        return new TRTriangle(a.sideLength * scalar);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        TRTriangle triangle1 = new TRTriangle();
+        TRTriangle triangle2 = new TRTriangle();
+
+        Console.WriteLine("Введiть данi для першого трикутника:");
+        triangle1.InputData();
+        Console.WriteLine("Введiть данi для другого трикутника:");
+        triangle2.InputData();
+
+        triangle1.OutputData();
+        triangle2.OutputData();
+
+        Console.WriteLine($"Площа трикутника 1: {triangle1.GetArea()}");
+        Console.WriteLine($"Периметр трикутника 1: {triangle1.GetPerimeter()}");
+
+        Console.WriteLine($"Трикутники рiвнi: {triangle1.IsEqual(triangle2)}");
+
+        TRTriangle triangle3 = triangle1 + triangle2;
+        TRTriangle triangle4 = triangle1 - triangle2;
+        TRTriangle triangle5 = triangle1 * 2;
+
+        triangle3.OutputData();
+        triangle4.OutputData();
+        triangle5.OutputData();
+    }
+}
